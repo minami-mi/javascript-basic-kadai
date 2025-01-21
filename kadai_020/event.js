669c55beba62fbd83208fd1b46cacd57daa91079
minami-mi <minami_yu_mi@icloud.com>
2025-01-21 21:51:50 +0900

    Delete kadai_020 directory

--- kadai_020/event.js
+++
@@ -1,9 +0,0 @@
-// btnというidを持つHTML要素を取得し、定数に代入する
-// textというidを持つHTML要素を取得し、定数に代入する
-const addBtn = document.getElementById('btn');
-const parentList = document.getElementById('text');
-
-// HTML要素がクリックされたときにイベント処理を実行する
-addBtn.addEventListener('click', () => {
-text.textContent = 'ボタンをクリックしました';
-});
